0f1fde6d9b3f3775c3addd36b3a9a4c48911b6ea
cacabo <ccabo@seas.upenn.edu>
2020-02-02 19:29:04 -0500

    refactor teams

---
+++ src/components/Team/Teams.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { ITeam, IMember } from '../../types'
+import { Fade, Section, H2, Row, Col, P } from '../../shared'
+import { M2 } from '../../constants/measurements'
+import { TeamMemberPreview } from './TeamMemberPreview'
+
+interface ITeams {
+  teams: ITeam[]
+}
+
+export const Teams = ({ teams }: ITeams) => (
+  <>
+    {teams.map(({ name, description, children: members }: ITeam) => (
+      <Fade key={name} distance="1rem">
+        <Section key={name}>
+          <H2 mb2>{name}</H2>
+          <Row>
+            <Col sm={12} md={10} lg={8}>
+              <P mb4>{description}</P>
+            </Col>
+          </Row>
+
+          <Row margin={M2}>
+            {members.map((props: IMember) => (
+              <TeamMemberPreview {...props} />
+            ))}
+          </Row>
+        </Section>
+      </Fade>
+    ))}
+  </>
+)
--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -3,11 +3,10 @@ import { useStaticQuery, graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
-import { Container, Section, P, Row, H1, H2, Col, Hero, Fade } from '../shared'
-import { M2 } from '../constants/measurements'
-import { TeamMemberPreview } from '../components/Team/TeamMemberPreview'
-import { ITeam, IMember } from '../types'
+import { Container, WideContainer } from '../shared'
+import { ITeam } from '../types'
 import { TeamHero } from '../components/Team/Hero'
+import { Teams } from '../components/Team/Teams'
 
 const AboutPage = (): React.ReactElement => {
   const {
@@ -46,26 +45,11 @@ const AboutPage = (): React.ReactElement => {
   return (
     <Layout>
       <SEO title="Team" />
-      <Container>
+      <WideContainer>
         <TeamHero />
-        {teams.map(({ name, description, children: members }: ITeam) => (
-          <Fade key={name} distance="1rem">
-            <Section key={name}>
-              <H2 mb2>{name}</H2>
-              <Row>
-                <Col sm={12} md={10} lg={8}>
-                  <P mb4>{description}</P>
-                </Col>
-              </Row>
-
-              <Row margin={M2}>
-                {members.map((props: IMember) => (
-                  <TeamMemberPreview {...props} />
-                ))}
-              </Row>
-            </Section>
-          </Fade>
-        ))}
+      </WideContainer>
+      <Container>
+        <Teams teams={teams} />
       </Container>
     </Layout>
   )
