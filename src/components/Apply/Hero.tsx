7d598b2b041c140b9ac62a7cbc6b41c262952f3d
valenciafu <valenciafu@gmail.com>
2020-02-05 22:51:43 -0500

    pcr

--- src/components/Apply/Hero.tsx
+++ src/components/Apply/Hero.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
 
-import {
-  Hero,
-  Img,
-  BtnAnchor,
-  LinkExternalLinkIcon,
-} from '../../shared'
+import { Hero, Img, BtnAnchor, LinkExternalLinkIcon } from '../../shared'
 import { APPLY_FORM_ROUTE } from '../../constants/routes'
 
 const deskPath = require('../../images/hero-resources.svg') as string // tslint:disable-line
@@ -14,12 +9,13 @@ export const ApplyHero = () => (
   <Hero
     title="Come Build with Us"
     subtitle="Join the Penn Labs community this spring"
-    body=
-      {APPLY_FORM_ROUTE && (
+    body={
+      APPLY_FORM_ROUTE && (
         <BtnAnchor href={APPLY_FORM_ROUTE} target="_BLANK">
           Apply Now <LinkExternalLinkIcon />
         </BtnAnchor>
-      )}
+      )
+    }
     Image={<Img src={deskPath} fullWidth mb0 />}
   />
 )
--- src/components/Apply/WhyLabs.tsx
+++ src/components/Apply/WhyLabs.tsx
@@ -10,27 +10,21 @@ export const WhyLabs = () => (
       Because we <span style={{ color: PINK }}>&hearts;</span> creators
     </H3>
     <P>
-      Penn Labs builds products that help students navigate life at Penn. All
-      Penn students are eligible to apply for any position, regardless of major
-      or year.
+      Penn Labs builds products that help students navigate life at Penn. Some
+      Penn Labs products that you may be familiar with include Penn Course
+      Review, Penn Mobile, and Penn Course Alert. All Penn students are eligible
+      to apply for any open position, regardless of their major or year.
     </P>
     <P>
-      Some Penn Labs products that you may be familiar with include Penn Course
-      Review, Penn Mobile, and Penn Course Alert. We’ve had more than 100,000
-      total users of our products. Through the Common Funding Application we
-      build and maintain, we've helped processed over $930,000 in grants for
-      student organizations.
-    </P>
-    <P>
-      Our mission is to build tech products to serve all Penn students, and we
-      recognize that goal requires a decent amount of commitment and technical
-      experience. We meet for 4 hours each week to work on projects, discuss
-      future product roadmaps, and learn more about the technologies we use.
-      Members are also expected to work outside of meetings to meet product
-      goals. While technical ability varies when joining Penn Labs and our
-      members are always learning, our commitment to building quality products
-      means that we seek candidates with sufficient technical ability, which we
-      evaluate through the technical challenge phase of the application process.
+      Our mission is to build tech products to serve all Penn students, a goal
+      which requires a both commitment and technical experience. Each week, we
+      meet for 4 hours to work on projects, discuss future product roadmaps, and
+      learn more about the technologies we use. Members are also expected to
+      work outside of meetings to meet product goals. While technical ability
+      varies when joining Penn Labs and our members are always learning, our
+      commitment to building quality products means that we seek candidates with
+      sufficient technical ability, which we evaluate through the technical
+      challenge phase of the application process.
     </P>
     <P>
       Come out to our info sessions to learn more and meet our members! And feel
