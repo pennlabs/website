82c5415bd5a82dd02e763bcfb304f7b224e3cc51
Peyton Walters <pawalt@hey.com>
2020-09-25 21:09:02 -0400

    Add club fair post

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -21,4 +21,4 @@ export const INFO_SESSIONS_EVENT_ROUTE = null
 export const STACK_ROUTE = 'https://stackshare.io/companies/penn-labs'
 export const FEEDBACK_ROUTE = 'https://airtable.com/shrwIkxqODSvOeLdh'
 export const MAILCHIMP_SUBSCRIBE_ROUTE =
-    'https://pennlabs.us11.list-manage.com/subscribe/post?u=302f80cd617d63a387d987ef2&amp;id=712d6085f5'
+  'https://pennlabs.us11.list-manage.com/subscribe/post?u=302f80cd617d63a387d987ef2&amp;id=712d6085f5'
