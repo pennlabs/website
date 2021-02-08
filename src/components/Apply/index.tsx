cf47dbaf4405d8d48964aac2618d53187f294c7a
Marta García Ferreiro <martagarciaferreiro@gmail.com>
2021-02-08 17:01:01 -0500

    Close applications s21 (#62)
    * open applications
    
    * retired armaan
    
    * close applications
    
    * remove package-lock.json
    
    * closing apps again

--- src/components/Apply/index.tsx
+++ src/components/Apply/index.tsx
@@ -10,7 +10,7 @@ export const Apply = () => (
   <>
     <ApplyHero />
     <WhyLabs />
-    <InfoSessions />
+    {/* <InfoSessions /> */}
     <OpenRoles />
     <ApplicationProcess />
     <More />
