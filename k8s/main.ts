3da1d47cb046ea9697d98e77b711df896aa246d6
Joy Liu <joyliu.q@gmail.com>
2022-06-22 22:04:34 -0400

    :art: new line

--- k8s/main.ts
+++ k8s/main.ts
@@ -1,8 +1,8 @@
-import { App } from 'cdk8s';
-import { ProductionChart } from './config/production';
-import { FeatureBranchChart } from './config/featureBranch';
+import { App } from 'cdk8s'
+import { ProductionChart } from './config/production'
+import { FeatureBranchChart } from './config/featureBranch'
 
-const isFeatureBranch =  process.env.DEPLOY_TO_FEATURE_BRANCH == "true"
-const app = new App();
-isFeatureBranch ? new FeatureBranchChart(app) : new ProductionChart(app);
-app.synth();
+const isFeatureBranch = process.env.DEPLOY_TO_FEATURE_BRANCH == 'true'
+const app = new App()
+isFeatureBranch ? new FeatureBranchChart(app) : new ProductionChart(app)
+app.synth()
