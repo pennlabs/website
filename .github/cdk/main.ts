38b33e9bccb30b1c00ae1d76e27a388aeef663ef
Joy Liu <joyliu.q@gmail.com>
2022-06-25 18:20:14 -0400

    :arrow_up: Upgrade deps

--- .github/cdk/main.ts
+++ .github/cdk/main.ts
@@ -1,23 +1,27 @@
-import { Construct } from "constructs";
-import { App, Stack, Workflow } from "cdkactions";
-import { DeployJob, NukeJob, ReactProject } from "@pennlabs/kraken";
+import { Construct } from 'constructs'
+import { App, Stack, Workflow } from 'cdkactions'
+import { DeployJob, NukeJob, ReactProject } from '@pennlabs/kraken'
 
 export class WebsiteStack extends Stack {
   constructor(scope: Construct, name: string) {
-    super(scope, name);
+    super(scope, name)
     const workflow = new Workflow(this, 'build-and-deploy', {
       name: 'Build and Deploy',
       on: 'push',
-    });
+    })
 
     const websiteJob = new ReactProject(workflow, {
       imageName: 'website',
-    });
+    })
 
     // Add Production Deploy
-    new DeployJob(workflow, {}, {
-      needs: [websiteJob.publishJobId]
-    });
+    new DeployJob(
+      workflow,
+      {},
+      {
+        needs: [websiteJob.publishJobId],
+      },
+    )
 
     // Add Feature Branch Deploy to Original Workflow
     new DeployJob(
@@ -26,23 +30,23 @@ export class WebsiteStack extends Stack {
         deployToFeatureBranch: true,
       },
       {
-        needs: [websiteJob.publishJobId]
-      }
-    );
+        needs: [websiteJob.publishJobId],
+      },
+    )
 
     // Create Feature Branch Nuke Worflow
     const featureBranchNukeWorkflow = new Workflow(
       this,
-      "feature-branch-nuke",
+      'feature-branch-nuke',
       {
-        name: "Feature Branch Nuke",
-        on: { pullRequest: { types: ["closed"] } },
-      }
-    );
-    new NukeJob(featureBranchNukeWorkflow, {}, {});
+        name: 'Feature Branch Nuke',
+        on: { pullRequest: { types: ['closed'] } },
+      },
+    )
+    new NukeJob(featureBranchNukeWorkflow, {}, {})
   }
 }
 
-const app = new App();
-new WebsiteStack(app, 'website');
-app.synth();
+const app = new App()
+new WebsiteStack(app, 'website')
+app.synth()
