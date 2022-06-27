5a9f7d6fcb1d4ddd9b1b5088529f7f93a1cef133
Joy Liu <joyliu.q@gmail.com>
2022-06-27 18:25:56 -0400

    :arrow_up: Bump kittyhawk

--- k8s/main.ts
+++ k8s/main.ts
@@ -2,7 +2,7 @@ import { App } from 'cdk8s'
 import { ProductionChart } from './config/production'
 import { FeatureBranchChart } from './config/featureBranch'
 
-const isFeatureBranch = process.env.DEPLOY_TO_FEATURE_BRANCH == 'true'
-const app = new App()
-isFeatureBranch ? new FeatureBranchChart(app) : new ProductionChart(app)
-app.synth()
+const isFeatureBranch = process.env.DEPLOY_TO_FEATURE_BRANCH == 'true';
+const app = new App();
+isFeatureBranch ? new FeatureBranchChart(app) : new ProductionChart(app);
+app.synth();
