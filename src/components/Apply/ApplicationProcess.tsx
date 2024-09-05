f35c8a2bba6065ea1d8fdc4ecbcc509ce6aa283c
Christopher Liu <liuchris@seas.upenn.edu>
2024-09-05 17:49:09 -0400

    Add info sessions for F24

--- src/components/Apply/ApplicationProcess.tsx
+++ src/components/Apply/ApplicationProcess.tsx
@@ -11,15 +11,15 @@ export const ApplicationProcess = () => (
     </P>
     <ol>
       <li>
+        <b>Technical challenge</b> where you can show off your skills. You’ll be
+        given a few days to complete a take-home challenge. We hold an office
+        hours event and are always available via email for questions.
+      </li>
+      <li>
         <b>Casual chat</b> with a current member, because we want to get to know
         you in person! Community and culture are very important to us. This
         conversation also gives you the chance to learn more about what being in
         Penn Labs is really like.
-      </li>
-      <li>
-        <b>Technical challenge</b> where you can show off your skills. You’ll be
-        given a few days to complete a take-home challenge. We hold an office
-        hours event and are always available via email for questions.
       </li>
     </ol>
   </Section>
